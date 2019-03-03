bd4c0c09e13d3e5e2603a74f9c7c755f30925be6
ppolena <PPolena92@gmail.com>
2019-03-03 20:51:22 +0100

    Set up postgres database connection, added postgres dependency, fixed CartItems entity missing annotations

--- backend/src/main/java/webshop/entity/CartItem.java
+++ backend/src/main/java/webshop/entity/CartItem.java
@@ -1,12 +1,23 @@
 package webshop.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-public class CartItem {
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "cart_items")
+public class CartItem extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
