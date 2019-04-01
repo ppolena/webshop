93293398c200c0218de5819daeb38072ea18c59c
Luca Hajdú <hajdu.luca@gmail.com>
2019-04-01 20:06:58 +0200

    First round of codes
    This is without routing. It is uploading for the basic looks with the code for the other frontend developer can process as well.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+import { AgmCoreModule } from '@agm/core';
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    }),
+    CarouselModule.forRoot(),
+    MatCardModule
+    
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
