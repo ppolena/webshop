c9f7f27cc328983d08a2a9a8c5114495328f95d9
ppolena <PPolena92@gmail.com>
2019-04-27 19:28:27 +0200

    fixed app.component.spec.ts missing declarations and imports

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,42 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueFormComponent } from './issue-form/issue-form.component';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CmspageModule } from './cmspage/cmspage.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatCardModule, MatListModule } from '@angular/material';
+import { AgmCoreModule } from '@agm/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel/public_api';
+import { RoutingModule } from './routing/routing.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        IssueListComponent,
+        IssueFormComponent,
+        IssueDetailComponent,
+        MainPageComponent
+      ],
+      imports: [
+        CmspageModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        MatButtonModule, 
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatIconModule,
+        AgmCoreModule.forRoot({
+          apiKey: 'YOUR_KEY'
+        }),
+        CarouselModule.forRoot(),
+        MatCardModule,
+        MatListModule,
+        RoutingModule
       ],
     }).compileComponents();
   }));
