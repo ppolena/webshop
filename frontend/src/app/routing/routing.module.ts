157f3571a0cec9fcf847c56e2e0a53c8d2490fc4
Luca Hajdú <hajdu.luca@gmail.com>
2019-04-28 19:23:18 +0200

    prettier code formatter
    #35

--- frontend/src/app/routing/routing.module.ts
+++ frontend/src/app/routing/routing.module.ts
@@ -1,24 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/issues',
-    pathMatch: 'full'
-  },
+    path: "",
+    redirectTo: "/issues",
+    pathMatch: "full"
+  }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [ 
-    RouterModule 
-  ],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   declarations: []
 })
-export class RoutingModule { }
+export class RoutingModule {}
