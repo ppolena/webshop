f44a531739bf43425ec02b37a843688869adb6c6
Luca Hajdú <hajdu.luca@gmail.com>
2019-05-19 21:08:12 +0200

    component.spec.ts for testing
    #53

--- frontend/src/app/shopping-form/shopping-form.component.spec.ts
+++ frontend/src/app/shopping-form/shopping-form.component.spec.ts
@@ -1,6 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatTableModule,
+  MatMenuModule,
+  MatSliderModule,
+  MatPaginatorModule,
+  MatGridListModule,
+  MatSelectModule,
+  MatInputModule,
+  MatRadioModule,
+  MAT_DIALOG_DATA,
+} from '@angular/material';
+
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
+
 import { ShoppingFormComponent } from './shopping-form.component';
 
 describe('ShoppingFormComponent', () => {
@@ -9,10 +33,16 @@ describe('ShoppingFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShoppingFormComponent ],
-      imports: [ RouterModule ]
-    })
-    .compileComponents();
+      declarations: [ShoppingFormComponent],
+      imports: [
+        RouterModule,
+        MatIconModule,
+        MatInputModule,
+        MatRadioModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
